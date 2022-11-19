84c108e51b8b2685c8826f35b98df945977fa704
Ida <ia.sokolow01@gmail.com>
2022-11-19 14:44:40 +0100

    feat: title added feat: new data added feat: added new component "employees-faces" feta: added service feat: added model

--- src/app/ui/employees-faces/employees-faces.component.ts
+++ src/app/ui/employees-faces/employees-faces.component.ts
@@ -1,7 +1,7 @@
 import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
 import { Observable } from 'rxjs';
 import { EmployeeService } from '../../services/employee.service';
-import { EmployeeModel } from '../../model/employee.model';
+import {PersonModel} from "../../model/person.model";
 
 @Component({
   selector: 'app-employees-faces',
@@ -13,6 +13,6 @@ export class EmployeesFacesComponent {
   constructor(private _employeeService: EmployeeService) {
 
   }
-  data$: Observable<EmployeeModel[]> = this._employeeService.getAll();
+  data$: Observable<PersonModel[]> = this._employeeService.getAll();
 }
 
