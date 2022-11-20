da1476f7d1dd23473f5069cec633a26d0c2e693c
Ida <ia.sokolow01@gmail.com>
2022-11-20 13:13:15 +0100

    feat: title added feat: new data added feat: added new component "employees-faces" feta: added service feat: added model feat: added form feat: added form alert

--- src/app/ui/employee-form/employee-form.component.ts
+++ src/app/ui/employee-form/employee-form.component.ts
@@ -8,5 +8,15 @@ import {FormControl, FormGroup, Validators} from '@angular/forms';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class EmployeeFormComponent {
-  readonly employeeForm: FormGroup = new FormGroup({ name: new FormControl(null, [Validators.required]), salary: new FormControl(null, [Validators.min(0), Validators.required]), age: new FormControl(null, [Validators.min(0)]) });
+  readonly employeeForm: FormGroup = new FormGroup({
+    name: new FormControl(null, [Validators.required]),
+    salary: new FormControl(null, [Validators.min(0), Validators.required]),
+    age: new FormControl(null, [Validators.min(0)]),
+    email: new FormControl(null, [Validators.required])
+  });
+
+  onButtonClick(form: { email: string, age: number, salary: number}) {
+    alert("User was successfully added to the database. Email: " + form.email + ", age: " + form.age + ", salary: " + form.salary)
+  }
 }
+
