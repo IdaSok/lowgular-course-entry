ee22c138825e9086253c1582c8d7015df0ce3c6c
Ida <ia.sokolow01@gmail.com>
2022-11-21 01:21:01 +0100

    feat: title added feat: new data added feat: added new component "employees-faces" feta: added service feat: added model feat: added form feat: added form alert feat: added operation to API feat: added remove function feat: added response

--- src/app/services/employee.service.ts
+++ src/app/services/employee.service.ts
@@ -3,6 +3,8 @@ import { HttpClient } from '@angular/common/http';
 import {map, Observable} from 'rxjs';
 import { PersonModel } from '../model/person.model';
 import { CreateEmployeeModel } from '../model/create-employee.model';
+import {ApiResponse} from './api.response';
+import {EmployeeResponse} from './employee.response';
 
 @Injectable()
 export class EmployeeService {
@@ -10,8 +12,18 @@ export class EmployeeService {
   }
 
   getAll(): Observable<PersonModel[]> {
-    return this._httpClient.get<PersonModel[]>('assets/data/people.json')
+    return this._httpClient.get<ApiResponse<EmployeeResponse[]>>('https://dummy.restapiexample.com/employees').pipe(map((response:ApiResponse<EmployeeResponse[]>): PersonModel[] => {
+        return response.data.map((employeeResponse:EmployeeResponse) => {
+          return {
+            personalNumber: employeeResponse.id,
+            name: employeeResponse.employee_name,
+            mail: employeeResponse.employee_name + "@lowgular.io",
+            img: employeeResponse.profile_image,
+          }
+        });
+        }));
   }
+
   create(employee: CreateEmployeeModel): Observable<any> {
     return this._httpClient.post('https://dummy.restapiexample.com/create', employee).pipe(map(_=>void 0));
   }
